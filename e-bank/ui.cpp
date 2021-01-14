b4f5c000aaf07bd48f59ed02b55466f98eb3c7fd
Adam <adamp-00@wp.pl>
2021-01-14 11:32:28 +0100

    .

--- e-bank/ui.cpp
+++ e-bank/ui.cpp
@@ -3,6 +3,7 @@
 #include "zaloguj.h"
 #include "operacja.h"
 #include "testy.h"
+#include "wyswietlane.h"
 
 void wczytaj_haslo(string &haslo) {
 	char znak;
@@ -38,47 +39,7 @@ void podan_dane_rejestruj(string& email, string& imie, string& haslo, string& na
 	cin >> nazwisko;
 }
 
-void menu() {
-	cout.width(60);
-	cout << "Aby sie zalogowac wybierz 1" << endl<<endl;
-	cout.width(64);
-	cout << "Aby sie zarejestrowac wybierz 2" << endl<<endl;
-	cout.width(56);
-	cout << "Aby zakonczyc wybierz 3" << endl << endl;
-}
 
-void menu_sprawdz() {			//wyrownac najlepiej w osobnym projekcie
-	cout.width(62);
-	cout << "Aby sprawdzic saldo wybierz 1" << endl << endl;
-	cout.width(64);
-	cout << "Aby sprawdzic kontaky wybierz 2" << endl << endl;
-	cout.width(65);
-	cout << "Aby sprawdzic historie wybierz 3" << endl << endl;
-	cout.width(68);
-	cout << "Aby sprawdzic kursy walut wybierz 4" << endl << endl;
-	cout.width(53);
-	cout << "Aby wrocic wybierz 0" << endl << endl;
-}
-
-void menu_wykonaj() {
-	cout.width(62);
-	cout << "Aby wykonac dowolny przelew 1" << endl << endl;
-	cout.width(69);
-	cout << "Aby wykonac przelew do kontrahenta 2" << endl << endl;
-	cout.width(57);
-	cout << "Aby dodac nowy kontakt 3" << endl << endl;
-	cout.width(54);
-	cout << "Aby wymienic waluty 4" << endl << endl;
-}
-
-void menu_operacji() {
-	cout.width(61);
-	cout << "Aby sprawdzic dane wybierz 1" << endl << endl;
-	cout.width(63);
-	cout << "Aby wykonac operacje wybierz 2" << endl << endl;
-	cout.width(60);
-	cout << "Aby wylogowac sie wybierz 3" << endl << endl;
-}
 
 
 
